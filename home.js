6cd1164e1a8b16b08cc121bf24b904ae751c48cc
jb <joyassroy@Joyassroys-MacBook-Air.local>
2025-08-27 02:20:43 +0600

    heart and call functions done

---
+++ home.js
@@ -0,0 +1,222 @@
+
+//for call button click
+document.getElementById('call-btn1').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling National Emergency 999...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn2').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Police 999...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn3').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Fire Service 999...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn4').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Ambulance 1994-999999...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn5').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Women & Child Helpline 109...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn6').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Anti-Corruption 106...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn7').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Electricity Outage 16216...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn8').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Brac 16445...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+document.getElementById('call-btn9').addEventListener('click',function(){
+    const point=document.getElementById('point-show').innerText;
+    const finalPoint=parseInt(point);
+
+    if(finalPoint<20){
+        alert("❌ You Dont have too much coin. You need atleast 20 point.")
+        return;
+    }
+    else{
+        alert('Calling Bangladesh Railway 163...');
+    }
+    document.getElementById('point-show').innerText=finalPoint-20;
+
+    
+});
+
+// for heart click
+
+document.getElementById('heart1').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart2').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart3').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart4').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart5').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart6').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart7').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart8').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+document.getElementById('heart9').addEventListener('click',function(){
+    const heartNumber=document.getElementById('heart-show').innerText;
+    let number=parseInt(heartNumber);
+    number=number+1;
+
+    
+   document.getElementById('heart-show').innerText=number;
+
+});
+
